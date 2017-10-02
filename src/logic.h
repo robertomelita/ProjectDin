02e26bf8daf2504bc69de03572219293a4e3f75b
Javier Rojas <javier.rojas02@alumnos.uach.cl>
2017-10-02 00:05:52 -0300

    logic.h

---
+++ src/logic.h
@@ -0,0 +1,15 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+	void logic();
+	//reglas del juego
+	//el personaje principal cuenta con 50 puntos de vida, la cual puede bajar tras recibir daño, o puede restaurarse
+	//si obtiene objetos especiales para ello.
+	//si la vida llega a 0, entonces se produce el fin del juego, lo cual deberá empezar en algun checkpoint
+	
+	//jugabilidad
+	//con un boton el personaje blandirá su espada para inflingir daño en un cono por delante de él a los enemigos.
+	//con otro boton el personaje usara su escudo para protegerse del daño de los enemigos.
+
+	//el juego se gana cuando se derrota al jefe final "dorfganon"
+	
+#endif
